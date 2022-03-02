cd631db27702c6cb292e03e241d757b1cc2a63d4
John_Gilbert <jmgilbert23@gmail.com>
2022-03-02 11:14:31 -0700

    Update tools.java
    started a temp console display for testing tools.

--- tools.java
+++ tools.java
@@ -8,4 +8,11 @@ Tools to implement our CSV file.
 
 public class tools {
     
+    public static void main(String[] args) {
+        
+        //Temp console display to test tools
+        System.out.println("Enter your choice:\n\t1. Display All\n\t2. Add Owner\n\t3. Remove Owner\n\t4. View Owner");
+    }
+
+
 }
