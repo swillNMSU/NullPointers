6eb5c7aa1c64dddf397f5785737e3dcfa7335ffa
Diego Morales <98107176+diegomorales30@users.noreply.github.com>
2022-03-02 13:36:18 -0700

    Created constructer and set and get methods

--- src/pet.java
+++ src/pet.java
@@ -8,6 +8,40 @@ VARS:
 */
 
 public class pet {
+    private boolean fixedPet;
+    private String name;
+    private String breed;
+
+
+    public pet(String nm, String brd){
+        this.name = nm;
+        this.breed = brd;
+        this.fixedPet = false;
+    }
+
+    public void setName(String nm) {
+        this.name = nm;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setFixedPet(boolean fixedPet) {
+        this.fixedPet = fixedPet;
+    }
+    
+    public boolean getFixedPet(){
+        return fixedPet;
+    }
+
     
 
 }
