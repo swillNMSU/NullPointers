f729668b38e83dcf31e3539298283fa25678c3e8
John_Gilbert <jmgilbert23@gmail.com>
2022-03-02 12:38:53 -0700

    Update owner.java
    added vars, initialized them, generated getters and setters. Made constructor for owner.

--- src/owner.java
+++ src/owner.java
@@ -1,4 +1,6 @@
 package src;
+
+
 /*
 Owner class. 
 Vars:
@@ -12,9 +14,75 @@ Vars:
 
 */
 
+
+
+/*
+* Constructor. Generates a default owner object with only the string 'name'.
+*/
 public class owner {
 
+    private String name, incomeProof;
+    private int numPets, strikes, numRecieved;
 
 
+    public owner(String _name){
+        setName(_name);
+        setIncomeProof(null);
+        setNumPets(0);
+        setStrikes(0);
+        setNumRecieved(0);
+    }
+
+//#region GET/SET
+    public int getNumRecieved() {
+        return numRecieved;
+    }
+
+
+    public void setNumRecieved(int numRecieved) {
+        this.numRecieved = numRecieved;
+    }
+
+
+    public int getStrikes() {
+        return strikes;
+    }
+
+
+    public void setStrikes(int strikes) {
+        this.strikes = strikes;
+    }
+
+
+    public int getNumPets() {
+        return numPets;
+    }
+
+
+    public void setNumPets(int numPets) {
+        this.numPets = numPets;
+    }
+
+
+    public String getIncomeProof() {
+        return incomeProof;
+    }
+
+
+    public void setIncomeProof(String incomeProof) {
+        this.incomeProof = incomeProof;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//#endregion
 
 }
