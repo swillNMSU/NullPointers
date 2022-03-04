3297c41ff8ba1ebbcc004167529e12290daf5410
swillNMSU <swill@nmsu.edu>
2022-03-03 17:41:18 -0700

    Update TestPet.java

---
+++ src/TestPet.java
@@ -0,0 +1,12 @@
+package src;
+
+import src.pet;
+
+public class TestPet {
+
+    public static void main (String[] args) {
+        pet test1 = new pet("Daisy", "Pitbull", "False");
+
+        System.out.println(test1.toString());
+    }
+}
