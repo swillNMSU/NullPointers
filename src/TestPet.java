e5b01dd8faa43d0973c541f4bf4c5f799106e125
swillNMSU <swill@nmsu.edu>
2022-03-03 19:02:30 -0700

    Testing Pet Class Functions

--- src/TestPet.java
+++ src/TestPet.java
@@ -1,12 +1,20 @@
 package src;
 
-import src.pet;
-
 public class TestPet {
 
     public static void main (String[] args) {
-        pet test1 = new pet("Daisy", "Pitbull", "False");
 
-        System.out.println(test1.toString());
+        System.out.println("Testing Pet Class:");
+        pet test1 = new pet("Daisy", "Pitbull", true);
+
+        System.out.println("First Test: " + test1.toString());
+
+        test1.setName("Caliber");
+        test1.setBreed("Poodle");
+        test1.setFixedPet(false);
+
+        System.out.println("\nUpdating name, breed and if fixed:" + test1.toString());
+
+        System.out.println("\nTesting Get Methods: " + "\n\tName: " + test1.getName() + "\n\tBreed: " + test1.getBreed() + "\n\tIf Fixed: " + test1.getFixedPet());
     }
 }
