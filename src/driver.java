aa37882c4ea0bb2ccef8562d4a6f9178a18b4fb6
John_Gilbert <85953852+JohnMGilbert@users.noreply.github.com>
2022-03-24 15:09:37 -0600

    Create driver.java
    Script to run our software. Contains global variable, a list of all owners.

---
+++ src/driver.java
@@ -0,0 +1,34 @@
+package src;
+import java.util.*;
+
+public class driver {
+
+    // global vars, for now just a list of all owners.
+    public static List<owner> owners = new ArrayList<>();
+
+    enum menu {
+        add,
+        remove,
+        display,
+        search
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        
+        // draw out all owners from the csv
+        read.readCSV();
+
+        for (owner owner : owners){
+            System.out.println("\n" + owner);
+
+            System.out.println("Total number of owners: " + owners.size());
+
+            System.out.println("Add owner: ");
+            write.addOwner();
+            
+            
+        }
+    }
+}
