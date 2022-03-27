dc2d42538337145f824d45e1bf187b9b87e15f3f
John_Gilbert <85953852+JohnMGilbert@users.noreply.github.com>
2022-03-27 16:25:36 -0600

    Update driver.java
    Added test code.

--- src/driver.java
+++ src/driver.java
@@ -16,6 +16,7 @@ public class Driver {
         archive
     }
 
+
     /**
      * Test driver
      * @param args
@@ -24,15 +25,33 @@ public class Driver {
        
        
         Scanner sc = new Scanner(System.in);
-       
+        System.out.println("Begin, type anything.");
+        sc.nextLine();
         Read.readCSV();
-        for (Owner owner : owners) System.out.println("\n" + owner);
+       // for (Owner owner : owners) System.out.println("\n" + owner);
         
-        System.out.println("Total number of owners: " + owners.size());
-        System.out.println("Add owner: ");
+         System.out.println("Total number of owners: " + owners.size());
+        // System.out.println("Add owner: ");
         
         Write.writeToCSV(writeFile);
-        Write.archiveCurrent();
+        // Write.archiveCurrent();
+       
+       
+// TEST: Edit method.
+
+
+
+
+// TEST: Search method and edit method.  
+        while(true){
+            System.out.print("Enter a name to search: ");
+            String s = sc.nextLine();
+            
+            List<Owner> r = Read.searchByName(s);
+            System.out.println("Possible results: ");
+            for (Owner x: r)
+                System.out.println("\t" + x.getName());
+        }
       
     }
 }
