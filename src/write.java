f9814e4e593ed5508a9bb82702069baf2a473271
John_Gilbert <85953852+JohnMGilbert@users.noreply.github.com>
2022-03-27 16:26:38 -0600

    Update write.java
    Added edit function, and enum for switch statement. Needs refactoring.

--- src/write.java
+++ src/write.java
@@ -8,6 +8,17 @@ import java.time.format.*;
 import java.util.Date;
 import java.lang.*;
 
+
+enum Edit{
+    name,
+    address,
+    numPets,
+    fixed,
+    incomeStatus,
+    strikes,
+    numRecieved
+}
+
 /**
  * All prints will be handled by the GUI in the future.
  */
@@ -68,6 +79,54 @@ public class Write {
         return Integer.parseInt(input);
     }
 
+   /**
+    * Changes data depending on what attribure is added to the parameters.
+    * @param ow the owner we will change the data of.
+    * @param edit case of data to be changed.
+    * @param newString 
+    * @param newNum
+    * @param newBool
+    */
+    public static void edit(Owner ow, Edit edit, String newString, int newNum, boolean newBool){
+        
+        switch (edit){
+            case name : 
+                ow.setName(newString);
+                break;
+            case address : 
+                ow.setAddress(newString);
+                break;
+            case numPets :
+                ow.setNumPets(newNum);
+                break;
+            case fixed :
+                ow.setIsFixed(newBool);
+                break;
+            case incomeStatus :
+                ow.setIncomeProof(newBool);
+                break;
+            case strikes :
+                ow.setStrikes(newNum);
+                break;
+            case numRecieved :
+                ow.setNumRecieved(newNum);
+                break;
+            default :
+                System.out.println("Error: Improper arguments.");
+
+                //TODO: Send something to the GUI
+
+        }
+        System.out.println("Save?  (Type exactly 'Yes')");
+        Scanner sc = new Scanner(System.in);
+        if (sc.nextLine().equals("Yes")) {
+            writeToCSV(Driver.writeFile); // This should not be called here. Testing purposes only
+
+            System.out.println("saved");
+        }
+        sc.close();
+    }
+
     /**
      * This method takes all owners stored and writes them to a csv file.
      * @param f, csv file to write owners to.
@@ -82,11 +141,11 @@ public class Write {
                 
                 String line = ow.getName() + "," + ow.getIncomeProof() + "," +
                     ow.getStrikes() + "," + ow.getNumRecieved() + "," + ow.getNumPets();
-                System.out.println(line);
+                
                 try {
                     fw.write(line + "\n");
                     fw.flush();
-                    System.out.println("Line added to CSV");
+                    System.out.println("Line added to CSV:  " + line);
                 }
                 catch (Exception e){
                     System.out.println("Line not written!");
@@ -139,4 +198,5 @@ public class Write {
         }
         
     }
+
 }
