c63e319fa68db4d9a30e32470468a9ef0ade7843
John_Gilbert <85953852+JohnMGilbert@users.noreply.github.com>
2022-04-08 08:58:52 -0600

    Update launch.json
    Added methods to keep add owner will all datavalidation

--- src/gui.java
+++ src/gui.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javafx.application.Application;
@@ -30,12 +31,15 @@ import javafx.stage.Stage;
 public class GUI extends Application {
 
     
-    Owner testOwner = new Owner("tFName tLName");
+  //  Owner testOwner = new Owner("tFName tLName");
     Owner selectedOwner;
     Stage ps;
     Scene mainMenu, editSc, addSc;
     ScrollPane sp = new ScrollPane();
     double width = 500, height = 600; // global sizes for scenes.
+    Validator dVal = new Validator();
+
+    TableView<Owner> owTable;
 
     // debugging feilds
     final boolean debug = true;
@@ -44,7 +48,6 @@ public class GUI extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
 /**
  * This sets the scene for the main menu.
  * FYI I'm using regions to make scrolling easier. In case you havent encountered them, 
@@ -54,12 +57,12 @@ public class GUI extends Application {
     public void start(Stage primaryStage) {
         
         // #region test owner - delete when done
-        testOwner.setAddress("1115 Branson Ave");
-        testOwner.setIncomeProof(true);
-        testOwner.setNumPets(2);
-        testOwner.setNumRecieved(1);
-        testOwner.setIsFixed(true);
-        testOwner.setStrikes(0);
+        // testOwner.setAddress("1115 Branson Ave");
+        // testOwner.setIncomeProof(true);
+        // testOwner.setNumPets(2);
+        // testOwner.setNumRecieved(1);
+        // testOwner.setIsFixed(true);
+        // testOwner.setStrikes(0);
         //#endregion
         
         ps = primaryStage;
@@ -68,7 +71,7 @@ public class GUI extends Application {
          //#region mainMenu
 
          //#region Table
-        TableView<Owner> owTable = new TableView<>();
+        owTable = new TableView<>();
         TableColumn<Owner, String> column1 = new TableColumn<>("Name");
         TableColumn<Owner, String> column2 = new TableColumn<>("Address");
         TableColumn<Owner, String> column3 = new TableColumn<>("Pets");
@@ -82,10 +85,6 @@ public class GUI extends Application {
         column4.setCellValueFactory(new PropertyValueFactory<>("strikes"));
         column5.setCellValueFactory(new PropertyValueFactory<>("numRecieved"));
         column6.setCellValueFactory(new PropertyValueFactory<>("isFixed"));
-
-    //     private String name, address;
-    // public int numPets, strikes, numRecieved;
-    // public boolean isFixed, incomeProof;
 
         owTable.getColumns().add(column1);
         owTable.getColumns().add(column2);
@@ -121,12 +120,6 @@ public class GUI extends Application {
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         grid.add(scenetitle, 1, 0, 2, 1);
 
-        // Button btn = new Button("Search");
-        // HBox hbBtn = new HBox(10);
-        // hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        // hbBtn.getChildren().add(btn);
-        // grid.add(hbBtn, 1, 4);
-
         Button addBtn = new Button("Add New");
         HBox tAddHB = new HBox(10);
         tAddHB.setAlignment(Pos.BOTTOM_RIGHT);
@@ -138,6 +131,9 @@ public class GUI extends Application {
 
         TextField searchTextField = new TextField();
         grid.add(searchTextField, 2, 7);
+
+        // add listener if row is selected.
+
 
         searchTextField.textProperty().addListener(new ChangeListener<String>()  {
             @Override
@@ -156,7 +152,12 @@ public class GUI extends Application {
         addBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e){
-                primaryStage.setScene(editSc);
+                Owner nOwner = new Owner();
+                Driver.owners.add(nOwner);
+                Write.writeToCSV(Driver.writeFile);
+                selectedOwner = nOwner;
+                ps.setScene(editSc);
+                initializeScenes();
             }
         });
 
@@ -170,14 +171,15 @@ public class GUI extends Application {
         editButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e){
-                selectedOwner = owTable.getSelectionModel().getSelectedItem();
-                //error check before swinthing scenes.
-                System.out.println(selectedOwner);
-                ps.setScene(editSc);
-                initializeScenes();
+                if (owTable.getSelectionModel().getSelectedItem() != null) {
+                    selectedOwner = owTable.getSelectionModel().getSelectedItem();
+                    //error check before swinthing scenes.
+                    System.out.println(selectedOwner);
+                    ps.setScene(editSc);
+                    initializeScenes();
+                }
             }
         });
-
 
         mainMenu = new Scene(grid, 300, 275);
         if (debug) {
@@ -188,6 +190,7 @@ public class GUI extends Application {
         primaryStage.show();
     }
 
+    
     public void initializeScenes(){
         Owner ow = new Owner("");
 
@@ -201,14 +204,27 @@ public class GUI extends Application {
         ps.setHeight(800);
         ps.setWidth(1000);
         GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER_LEFT);
+        grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-        Text scenetitle = new Text("Edit Owner Information");
+        Text scenetitle = new Text("Owner Information");
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         grid.add(scenetitle, 0, 0, 2, 1);
+
+        // BUTTONS
+        Button saveBtn = new Button("Save");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(saveBtn);
+        grid.add(hbBtn, 1, 7);
+
+        Button backBtn = new Button("Back");
+        HBox bHitB = new HBox(10);
+        bHitB.setAlignment(Pos.BOTTOM_LEFT);
+        bHitB.getChildren().add(backBtn);
+        grid.add(bHitB, 0, 7);
 
         //#region name
         Label ownerNameL = new Label("Name:");
@@ -218,6 +234,7 @@ public class GUI extends Application {
         grid.add(ownerName, 1, 1);
 
         TextField ownerTextField = new TextField(ow.getName()); 
+        Text nameErr = displayErr(grid, "Invalid name", 5, 1);
         
 
         Button editBtn0 = new Button("Edit");
@@ -231,8 +248,6 @@ public class GUI extends Application {
             public void handle(ActionEvent e){
                 ownerName.setVisible(false);
                 grid.add(ownerTextField, 1, 1);
-                // select text field
-                //highlight text field
             }
         });
         //#endregion
@@ -246,6 +261,8 @@ public class GUI extends Application {
 
         TextField addressTextField = new TextField(ow.getAddress());
 
+        Text addrErr = displayErr(grid, "Invalid address", 5, 2);
+
         Button editBtn1 = new Button("Edit");
         HBox editHB1 = new HBox(10);
         editHB1.setAlignment(Pos.CENTER_LEFT);
@@ -257,8 +274,6 @@ public class GUI extends Application {
             public void handle(ActionEvent e){
                 ownerAddr.setVisible(false);
                 grid.add(addressTextField, 1, 2);
-                // select text field
-                //highlight text field
             }
         });
 
@@ -273,6 +288,8 @@ public class GUI extends Application {
 
         TextField numPeTextField = new TextField(String.valueOf(ow.getNumPets()));
 
+        Text petErr = displayErr(grid, "Invalid entry", 5, 3);
+
         Button editBtn2 = new Button("Edit");
         HBox editHB2 = new HBox(10);
         editHB2.setAlignment(Pos.CENTER_LEFT);
@@ -284,8 +301,6 @@ public class GUI extends Application {
             public void handle(ActionEvent e){
                 ownerNumPets.setVisible(false);
                 grid.add(numPeTextField, 1, 3);
-                // select text field
-                //highlight text field
             }
         });
 
@@ -296,17 +311,19 @@ public class GUI extends Application {
         grid.add(numStrikesL, 0, 4);
 
         Label ownerStrikes = new Label(String.valueOf(ow.getStrikes()));
+        grid.add(ownerStrikes, 1, 4);
 
-        TextField numStrikesTextField = new TextField();
-        grid.add(numStrikesTextField, 1, 4);
+        TextField numStrikesTextField = new TextField(String.valueOf(ow.getStrikes()));
+        
+        Text strikeErr = displayErr(grid, "*", 5, 4);
 
         Button editBtn3 = new Button("Edit");
         HBox editHB3 = new HBox(10);
-        editHB3.setAlignment(Pos.CENTER);
+        editHB3.setAlignment(Pos.CENTER_LEFT);
         editHB3.getChildren().add(editBtn3);
         grid.add(editHB3, 3, 4);
 
-        editBtn2.setOnAction(new EventHandler<ActionEvent>() {
+        editBtn3.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e){
                 ownerStrikes.setVisible(false);
@@ -319,61 +336,109 @@ public class GUI extends Application {
         //#endregion
 
         //#region Withdrawls
-        Label numWithdrawls = new Label("Withdrawls:");
-        grid.add(numWithdrawls, 0, 5);
+        Label numWithdrawlsL = new Label("Withdrawls:");
+        grid.add(numWithdrawlsL, 0, 5);
 
-        TextField numWithdrawlsTextField = new TextField();
-        grid.add(numWithdrawlsTextField, 1, 5);
+        Label numWithdrawls = new Label(String.valueOf(ow.getNumRecieved()));
+        grid.add(numWithdrawls, 1, 5);
 
+        TextField numWithdrawlsTextField = new TextField(String.valueOf(ow.getNumRecieved()));
+
+        Text withdrawlErr = displayErr(grid, "*", 5, 5);
+    
         Button editBtn4 = new Button("Edit");
         HBox editHB4 = new HBox(10);
         editHB4.setAlignment(Pos.CENTER_LEFT);
         editHB4.getChildren().add(editBtn4);
         grid.add(editHB4, 3, 5);
 
+        editBtn4.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e){
+                numWithdrawls.setVisible(false);
+                grid.add(numWithdrawlsTextField, 1, 5);
+            }
+        });
+
         //#endregion
         
         //#region save button
-        Button saveBtn = new Button("Save");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(saveBtn);
-        grid.add(hbBtn, 1, 7);
-        //#endregion
-        //#region back button
-        Button backBtn = new Button("Back");
-        HBox bHitB = new HBox(10);
-        bHitB.setAlignment(Pos.BOTTOM_LEFT);
-        bHitB.getChildren().add(backBtn);
-        grid.add(bHitB, 0, 7);
 
         backBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e){
+                // Confirm if data hasn't been saved
                 System.out.println("Back to Main menu.");
                 ps.setScene(mainMenu);
+                owTable.getItems().clear();
+                for (Owner ows : Driver.owners)
+                    owTable.getItems().add(ows);
             }
         });
         //#endregion
         
-        final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 6);
+        Text isSaved = new Text("Saved");
+        grid.add(isSaved, 1, 6);
+        isSaved.setVisible(false);
+        isSaved.setFill(Color.BLUEVIOLET);
+
 
         saveBtn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent e) { // IF there is at least a name entered.
-                if (ownerTextField.getText() != null){
-                    Owner tempOwn = new Owner();
-                    tempOwn.setName(ownerTextField.getText());
-                    tempOwn.setAddress(addressTextField.getText());
-                    tempOwn.setNumPets(Integer.parseInt(numPeTextField.getText()));
-                    tempOwn.setStrikes(Integer.parseInt(numStrikesTextField.getText()));
-                    System.out.println(tempOwn);
+                boolean canSave = true;
+                //TODO add fixed status to DVAL
+                
+                if (dVal.checkNameFields(ownerTextField.getText())){
+                    nameErr.setVisible(true);
+                    canSave = false;
+                } else {nameErr.setVisible(false);}
+
+                if (dVal.checkAddress(addressTextField.getText())){
+                    addrErr.setVisible(true);
+                    canSave = false;
+                } else {addrErr.setVisible(false);}
+
+                if (dVal.checkNumPets(numPeTextField.getText())){
+                    petErr.setVisible(true);
+                    canSave = false;
+                } else {petErr.setVisible(false);}
+
+                if (dVal.checkPickUps(numStrikesTextField.getText())){ //TODO
+                    strikeErr.setVisible(true);
+                    canSave = false;
+                } else { strikeErr.setVisible(false);}
+                
+                if (dVal.checkPickUps(numWithdrawlsTextField.getText())){
+                    withdrawlErr.setVisible(true);
+                    canSave = false;
+                } else { withdrawlErr.setVisible(false);}
+                
+                if (!canSave){
+                    isSaved.setText("Invalid entry");
+                    isSaved.setVisible(true);
                 }
-                ownerTextField.getText();
-                actiontarget.setFill(Color.FIREBRICK);
-                actiontarget.setText("Saved");
+                else {
+                    owTable.getSelectionModel().getSelectedItem().setAllFeilds(
+                        ownerTextField.getText(),
+                        addressTextField.getText(),
+                        true,
+                        true,
+                        Integer.parseInt(numPeTextField.getText()),
+                        Integer.parseInt(numWithdrawls.getText())
+                        );
+
+                    System.out.println(owTable.getSelectionModel().getSelectedItem());
+                    isSaved.setText("Saved");
+                    isSaved.setVisible(true);
+                    // Driver.owners.clear();
+                    // for (Owner ow : owTable.getItems())
+                    // {
+                    //     Driver.owners.add(ow);
+                    // }
+                    Write.writeToCSV(Driver.writeFile);
+                }
             }
         });
 
@@ -381,8 +446,11 @@ public class GUI extends Application {
         ps.setScene(editSc);
         ps.show();
        
-       
         //#endregion
+
+        // addSc = new Scene(grid, 300, 275);
+        // ps.setScene(addSc);
+        // ps.show();
     }
 
     public Text displayErr(GridPane gr, String err, int width, int height){
@@ -395,5 +463,3 @@ public class GUI extends Application {
     }
 
 }
-
-
