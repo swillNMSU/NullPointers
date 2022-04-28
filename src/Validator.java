c301a0c1c2a0be4247ea4962582d962227204986
John_Gilbert <85953852+JohnMGilbert@users.noreply.github.com>
2022-04-28 11:54:23 -0600

    Added Diegos strike validator

--- src/Validator.java
+++ src/Validator.java
@@ -1,39 +1,39 @@
 package src;
 
 public class Validator {
-    static boolean isEmptyInput(String isItEmpty){
-        if(isItEmpty.isEmpty() || isItEmpty.isBlank()){
+    static boolean isEmptyInput(String isItEmpty) {
+        if (isItEmpty.isEmpty() || isItEmpty.isBlank()) {
             System.out.println("EMPTY INPUT NOT ALLOWED");
             return true;
         }
         return false;
     }
 
-    static boolean isNegative(String isItNegative){
+    static boolean isNegative(String isItNegative) {
         int checkStrtoNum = Integer.parseInt(isItNegative);
-        if(checkStrtoNum < 0){
+        if (checkStrtoNum < 0) {
             System.out.println("Negative numbers are not allowed");
             return true;
         }
         return false;
     }
-    
-    public boolean checkNameFields(String isValidName){
+
+    public boolean checkNameFields(String isValidName) {
         boolean notValid = true;
         int invalidIntex = 1;
 
-        if(isEmptyInput(isValidName))
+        if (isEmptyInput(isValidName))
             return true;
 
-        for(int i = 0; i < isValidName.length() && notValid;i++){
-            if(isValidName.charAt(i) != ' ' && (isValidName.charAt(i) < 'A' || isValidName.charAt(i) > 'Z') && 
-            (isValidName.charAt(i) < 'a' || isValidName.charAt(i) > 'z')){
+        for (int i = 0; i < isValidName.length() && notValid; i++) {
+            if (isValidName.charAt(i) != ' ' && (isValidName.charAt(i) < 'A' || isValidName.charAt(i) > 'Z') &&
+                    (isValidName.charAt(i) < 'a' || isValidName.charAt(i) > 'z')) {
                 notValid = false;
                 invalidIntex = i + 1;
             }
         }
 
-        if(!notValid){
+        if (!notValid) {
             System.out.print("YOU ENTERED A VALUE THAT WAS NOT \"A-Z\" or \"a-z\": ");
             System.out.println("The error is at " + invalidIntex);
             return true;
@@ -44,84 +44,124 @@ public class Validator {
     public boolean checkPickUps(String isValidNum) {
         boolean notValidPickUps = true;
         int errorIndex = 1;
-        if(isEmptyInput(isValidNum))
+        if (isEmptyInput(isValidNum))
             return true;
 
-        for(int i = 0; i < isValidNum.length() && notValidPickUps;i++){
-            if(isValidNum.charAt(0) == '-'){
+        for (int i = 0; i < isValidNum.length() && notValidPickUps; i++) {
+            if (isValidNum.charAt(0) == '-') {
                 System.out.print("No negative numbers are allowed: ");
                 System.out.println("Error at " + errorIndex);
                 return true;
             }
 
-            if(isValidNum.charAt(i) < '0' || isValidNum.charAt(i) > '9'){
+            if (isValidNum.charAt(i) < '0' || isValidNum.charAt(i) > '9') {
                 errorIndex = errorIndex + i;
                 notValidPickUps = false;
             }
         }
 
-        if(!notValidPickUps){
+        if (!notValidPickUps) {
             System.out.print("You can not have letter or any symboles in this field: ");
             System.out.println("Error at " + errorIndex);
             return true;
         }
-    
+
         int isOkayNumInput = Integer.parseInt(isValidNum);
-        if(isOkayNumInput > 8){
+        if (isOkayNumInput > 8) {
             System.out.println("You can no longer get food until next year");
             return true;
         }
         return false;
     }
 
-    public boolean checkNumPets(String isValidNumPets){
+    // this is what is needed for checking the strikes
+    static boolean checkStrikes(String isValidStrikes) {
+        boolean isInValid = true;
+        int invalidInput = 1;
+        // need to check if empty
+        if (isValidStrikes.isEmpty()) {
+            System.out.println("You can not have an empty value");
+            return true;
+        }
+        // if string not empty then go through each index find the error
+        for (int i = 0; i < isValidStrikes.length() && isInValid; i++) {
+            if (isValidStrikes.charAt(0) == '-') {
+                System.out.print("Negative numbers are not allowed: ");
+                System.out.println("Error at " + invalidInput);
+                return true;
+            }
+            if (isValidStrikes.charAt(i) < '0' || isValidStrikes.charAt(i) > '9') {
+                invalidInput = i + invalidInput;
+                isInValid = false;
+            }
+        }
+        // first if to check if the method is invalid from the string.
+        if (!isInValid) {
+            System.out.print("You have an invalid input at " + invalidInput);
+            return true;
+        }
+        // The string of input is okay
+        int checkNumber = Integer.parseInt(isValidStrikes);
+        if (checkNumber < 0) {
+            System.out.println("You can not have a negative number of strikes");
+            return true;
+        }
+        else if (checkNumber > 3) {
+            System.out.println("You can not get any pet food after you show proof");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkNumPets(String isValidNumPets) {
         boolean notValidPetNum = true;
         int errorIndex = 1;
-        if(isEmptyInput(isValidNumPets))
+        if (isEmptyInput(isValidNumPets))
             return true;
 
-        for(int i = 0; i < isValidNumPets.length() && notValidPetNum;i++){
-            if(isValidNumPets.charAt(0) == '-'){
+        for (int i = 0; i < isValidNumPets.length() && notValidPetNum; i++) {
+            if (isValidNumPets.charAt(0) == '-') {
                 System.out.print("Negative numbers are not allowed: ");
                 System.out.println("Error at " + errorIndex);
                 return true;
             }
-            if(isValidNumPets.charAt(i) < '0' || isValidNumPets.charAt(i) > '9'){
+            if (isValidNumPets.charAt(i) < '0' || isValidNumPets.charAt(i) > '9') {
                 notValidPetNum = false;
                 errorIndex = errorIndex + i;
             }
         }
-        if(!notValidPetNum){
+        if (!notValidPetNum) {
             System.out.print("Invalid input can not have letters or characters: ");
             System.out.println("Error at " + errorIndex);
             return true;
         }
 
         int isOkayNumInput = Integer.parseInt(isValidNumPets);
-        if(isOkayNumInput == 0){
+        if (isOkayNumInput == 0) {
             System.out.println("You can not have 0 pets");
             return true;
         }
         return false;
     }
 
-    public boolean checkAddress(String isValidAddress){
+    public boolean checkAddress(String isValidAddress) {
         boolean notValidAddress = true;
         int invalidIndex = 1;
-        if(isEmptyInput(isValidAddress))
+        if (isEmptyInput(isValidAddress))
             return true;
 
-        for(int i = 0; i < isValidAddress.length() && notValidAddress; i++){
-            if(isValidAddress.charAt(i) != ' ' && (isValidAddress.charAt(i) < 'A' || isValidAddress.charAt(i) > 'Z')
-                && (isValidAddress.charAt(i) < 'a' || isValidAddress.charAt(i) > 'z') && (isValidAddress.charAt(i) < '0' || 
-                isValidAddress.charAt(i) > '9') && isValidAddress.charAt(i) != '.'
-            ){
+        for (int i = 0; i < isValidAddress.length() && notValidAddress; i++) {
+            if (isValidAddress.charAt(i) != ' ' && (isValidAddress.charAt(i) < 'A' || isValidAddress.charAt(i) > 'Z')
+                    && (isValidAddress.charAt(i) < 'a' || isValidAddress.charAt(i) > 'z')
+                    && (isValidAddress.charAt(i) < '0' ||
+                            isValidAddress.charAt(i) > '9')
+                    && isValidAddress.charAt(i) != '.') {
                 notValidAddress = false;
                 invalidIndex = i + invalidIndex;
             }
         }
 
-        if(!notValidAddress){
+        if (!notValidAddress) {
             System.out.print("Invalid Addres put: ");
             System.out.println("Error at " + invalidIndex);
             return true;
