31cbeda88dabb099555ee49ac0a7480932b5a68f
Diego Morales <98107176+diegomorales30@users.noreply.github.com>
2022-05-05 12:09:12 -0600

    Add commets to each method

--- datavalidation/driver.java
+++ datavalidation/driver.java
@@ -1,6 +1,11 @@
 package src.datavalidation;
 
 import java.util.*;
+
+/**
+ *  This Driver class was to test the DataTest to make sure that the methods are working
+ *  correctly on catching invalid data.
+ */
 public class driver{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -16,6 +21,20 @@ public class driver{
         
     }
 
+    /**
+     * 
+     * @param name
+     *  input scanner that is a string
+     * @param message
+     *  The message for the user input thier name
+     * @param personsInfoOwner
+     *  The object that I was checking to make sure that the information is working correctly
+     *  
+     *  This method methods job is to make sure that the user is inputing correct data if the 
+     *  data is incorrect then the while loop will keep running until the input is valid. The 
+     *  DataTest.checkNameField method returns a booleen value true for invalid and false for
+     *  valid. It will update the owners name filed.
+     */
     public static void getPersontName(Scanner name,String message, owner personsInfoOwner){
         System.out.println(message);
         String inputname = name.nextLine();
@@ -27,7 +46,20 @@ public class driver{
         personsInfoOwner.setName(inputname);
     }
 
-    
+    /**
+     * 
+     * @param incom
+     *  String input from the user
+     * @param message
+     *  The message that tells the user what to up
+     * @param personInfo
+     *  The information that is being updated when the data is valid
+     *  
+     *  This methods purpose is to get the persons inforamtion and make sure that the user entered
+     *  the correct inforamtion. The class DataTest with the method checkNameField will validat the
+     *  user input. The loop will not break unitl false if false then the data is valid and update the 
+     * income proof of the owner . If true it will keep on prompting the user to enter until valid.
+     */
     public static void getPersonIncome(Scanner incom, String message,owner personInfo){
         System.out.println(message);
         String incomeInput = incom.nextLine();
@@ -38,6 +70,20 @@ public class driver{
         personInfo.setIncomeProof(incomeInput);
     }
 
+    /**
+     * 
+     * @param numPet
+     *  String input from user
+     * @param message
+     *  To tell the user what to put
+     * @param personInfo
+     *  Updating owners inforamtion
+     * 
+     *  This method purpose is to get user input for the number of pets. The class DataTest uses
+     *  checkNumPets to check if the number that the user input is a valid input, if true then the
+     *  input is invalid. If the data is valid then it will be false. Will update the number of pets
+     *  in the owners pets filed
+     */
     public static void getNumPets(Scanner numPet, String message, owner personInfo){
         System.out.println(message);
         String input = numPet.nextLine();
@@ -49,6 +95,20 @@ public class driver{
         personInfo.setNumPets(validNumInput);
     }
 
+    /**
+     * 
+     * @param petFoodGiven
+     *  String input from the user
+     * @param message
+     *  Tells the user what to put
+     * @param personInfo
+     *  Updated the owners pick ups
+     * 
+     *  The purpose of this method is to make sure that the user is inputing the correct input
+     *  for this field. If the the data input for this method is invalid then the while loop 
+     *  will keep on running. If false then the input is valid. It will updat the recieved filed
+     *  in the owner class
+     */
     public static void getNumPickUp(Scanner petFoodGiven, String message, owner personInfo) {
         System.out.println(message);
         String input = petFoodGiven.nextLine();
@@ -61,6 +121,19 @@ public class driver{
     }
 
 
+    /**
+     * 
+     * @param addressInput
+     *  String input for the address
+     * @param message
+     *  Message for the user to input the address
+     * @param personInfo
+     *  Update the user information for thier address
+     * 
+     *  The purpose for this method to make sure that the information that the user is inputting
+     *  is correct for this field. If the data is invalid then the while loop will keep on running
+     *  if the data is valid then the loop breaks and updates the owners address field.
+     */
     public static void getAddress(Scanner addressInput, String message,owner personInfo){
         System.out.println(message);
         String input = addressInput.nextLine();
@@ -70,6 +143,17 @@ public class driver{
         }
     }
 
+    /**
+     * 
+     * @param strikesInput
+     *  Input for strikes
+     * @param message
+     *  Message to tell the user what to input
+     * 
+     *  This methods purpos is to make sure that the input that the user is putting is correct
+     *  if the the data is invalid then the loop will continu until input is valid. If valid then
+     *  the loop will break.
+     */
     public static void getStrikes(Scanner strikesInput, String message){
         System.out.println(message);
         String input = strikesInput.nextLine();
