a7c2c05de0080a846c68ed3c78f258f6d27b3e5f
John_Gilbert <85953852+JohnMGilbert@users.noreply.github.com>
2022-05-07 11:33:30 -0600

    Update Driver.java
    Just added a comment

--- src/Driver.java
+++ src/Driver.java
@@ -3,14 +3,14 @@ import java.io.File;
 import java.util.*;
 
 /**
- * This class hold static variables accessed by all other classes.
+ * This class holds static variables accessed by all other classes.
  */
 public class Driver {
 
   public static List<Owner> owners = new ArrayList<>();
   public static List<Owner> currentArchives = new ArrayList<>();
   public static File writeFile = new File("src/testReset.csv");
-  public static String errorMessage;
+  public static String errorMessage; // passed through here when an error is dependant on two classes. Always set to null after use.
 
 
 }
